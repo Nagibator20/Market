96f1472a05da020e30d9d0ca736f8d68eb133531
Nagibator20 <Ja_mayka@mail.ru>
2023-02-20 18:41:27 +0200

    finish

--- src/js/files/forms/range.js
+++ src/js/files/forms/range.js
@@ -10,29 +10,33 @@ import * as noUiSlider from 'nouislider'
 export function rangeInit() {
 	//! Дописанный код как в видео
 	const rangeItems = document.querySelectorAll('[data-range]')
+
 	if (rangeItems) {
 		rangeItems.forEach((rangeItems) => {
 			const fromValue = rangeItems.querySelector('[data-range-from]')
 			const toValue = rangeItems.querySelector('[data-range-to]')
 			const item = rangeItems.querySelector('[data-range-item]')
+			const inputs = [fromValue, toValue]
+
 			//! UI слайдер
 			// !!! преобразовать строку в число -> Number(то что нужно преобразовать)
-			console.log(Math.round(Number(fromValue.value)))
+			// console.log(Math.round(Number(fromValue.value)))
 			// console.dir(item)
-			console.dir(rangeItems)
+			// console.dir(rangeItems)
 			noUiSlider.create(item, {
-				start: [Number(fromValue.value),Number(toValue.value)], // [0,200000]
+				start: [Number(fromValue.value), Number(toValue.value)], // [0,200000]
 				connect: true, //! [true false] было
 				tooltips: [true, true], //! активация подсказок. Скопировал с сайта (range)
 				// step:1,
-				format: {//!!! округляет в тултипе значение до целого значения!!!
-					to: function ( value ) {
-						const aw = value;
-						return Math.round(aw);
+				format: {
+					//!!! округляет в тултипе значение до целого значения!!!
+					to: function (value) {
+						const aw = value
+						return Math.round(aw)
 					},
-					from: function ( value ) {
-						return value;
-					}
+					from: function (value) {
+						return value
+					},
 				},
 				range: {
 					min: [Number(fromValue.dataset.rangeFrom)],
@@ -41,22 +45,67 @@ export function rangeInit() {
 					// max: [toValue.value],
 				},
 			})
+			//!! добавил ввод значений вручную
+			inputs.forEach(function (input, handle) {
+				input.addEventListener('change', function () {
+					item.noUiSlider.setHandle(handle, this.value)
+				})
+				input.addEventListener('keydown', function (e) {
+					const values = item.noUiSlider.get()
+					const value = Number(values[handle])
+					// [[handle0_down, handle0_up], [handle1_down, handle1_up]]
+					const steps = item.noUiSlider.steps()
+					// [down, up]
+					const step = steps[handle]
+					let position;
+					// 13 is enter,
+					// 38 is key up,
+					// 40 is key down.
+					switch (e.which) {
+						case 13:
+							item.noUiSlider.setHandle(handle, this.value)
+							break
+						case 38:
+							// Get step to go increase slider value (up)
+							position = step[1]
+							// false = no step is set
+							if (position === false) {
+								position = 1
+							}
+							// null = edge of slider
+							if (position !== null) {
+								item.noUiSlider.setHandle(handle, value + position)
+							}
+							break
+						case 40:
+							position = step[0]
+							if (position === false) {
+								position = 1
+							}
+							if (position !== null) {
+								item.noUiSlider.setHandle(handle, value - position)
+							}
+							break
+					}
+				})
+			})
 			// ! привязка значений к ползунку (скопировано с сайта и доработано)
 			//! так в видео окна с диапазоном работают неправильно
-			// console.dir(item) 
+			// console.dir(item)
 			// item.noUiSlider.on('update', function (values, handle) {
 			// 	fromValue.value = values[handle]
 			// 	toValue.value = values[handle]
 			// })
 			// ! переписал сам что бы работали 2 окна с диапазоном правильно
 			// ! переписать что бы округляло значения!!!!
-			item.noUiSlider.on('update', function (values, handle,unencoded) {
+			item.noUiSlider.on('update', function (values, handle) {
+				inputs[handle].value = values[handle]
 				if (handle) {
-					toValue.value = Math.round(values[handle]);
+					toValue.value = Math.round(values[handle])
 				} else {
-					fromValue.value = Math.round(values[handle]);
+					fromValue.value = Math.round(values[handle])
 				}
-		},);
+			})
 		})
 	}
 	//!======================
